513012d24176d7ee1cf3123c24225add4807364f
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-02-21 13:34:49 +0530

    Book Rendering with Pages

--- Book.h
+++ Book.h
@@ -1,9 +1,34 @@
-#include<GL/glut.h>
+#include "Page.h"
 class Book
 {
     public:
-        GLfloat width;
-        GLfloat height;
-        GLint noOfPages;
-        GLint currentPage;
+    	GLfloat x, y, z;
+        GLfloat width, height;
+        GLint noOfPages, currentPageIndex;
+        Page **pages;
+        Book(GLfloat, GLfloat, GLfloat);
+        void addPage(GLint);
+        void renderPage();
+        void renderPage(GLint);
 };
+Book::Book(GLfloat x, GLfloat y, GLfloat z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	this->noOfPages = 0;
+}
+void Book::addPage(GLint type)
+{
+	pages[noOfPages]=new Page(type, x + BOOK_BORDER_SIZE, y + BOOK_BORDER_SIZE, noOfPages);
+	currentPageIndex = noOfPages++;
+}
+void Book::renderPage()
+{
+	pages[currentPageIndex]->renderPage();
+}
+void Book::renderPage(GLint pageIndex)
+{
+	if(pageIndex<noOfPages)
+		pages[pageIndex]->renderPage();
+}
