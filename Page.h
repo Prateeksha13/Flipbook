5a6ed588875400ff0a587c8a4c5da31743171608
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-02-21 13:35:56 +0530

    Flipbook Generated with Pages

--- Page.h
+++ Page.h
@@ -1,43 +1,57 @@
 class Page
 {
     public:
-        GLfloat width;
-        GLfloat height;
-        GLfloat noOfLines;
         GLint type;
-        GLfloat currentLine;
-        GLfloat lines[MAX_LINES];
-        Page(GLint);
+        GLfloat x, y, z;
+        GLfloat width, height;
+        GLfloat marginWidth, marginHeight;
+        GLfloat lineHeight, noOfLines, currentLine, lines[MAX_LINES];
+        Page(GLint, GLfloat, GLfloat, GLfloat);
         void renderPage();
         void setMargin();
         void setPageLines();
+        void setBorder();
 };
-Page::Page(GLint type)
+Page::Page(GLint type, GLfloat x, GLfloat y, GLfloat z)
 {
+    this->x = x;
+    this->y = y;
+    this->z = z;
     this->type = type;
-    renderPage();
+    this->width = PAGE_WIDTH;
+    this->height = PAGE_HEIGHT;
+    this->marginWidth = MARGIN_WIDTH;
+    this->marginHeight = MARGIN_HEIGHT;
+    this->lineHeight = LINE_HEIGHT;
+}
+void Page::renderPage()
+{
+    glFlush();
+    glColor3f(0.0,0.0,0.0);
+    setBorder();
     setMargin();
     if(type == PAGE_TYPE_TEXT)
         setPageLines();
 }
-void Page::renderPage()
+void Page::setBorder()
 {
     glBegin(GL_LINE_LOOP);
-        glVertex3f(0.0,0.0,0.0);
-        glVertex3f(PAGE_WIDTH,0.0,0.0);
-        glVertex3f(PAGE_WIDTH,PAGE_HEIGHT,0.0);
-        glVertex3f(0.0,PAGE_HEIGHT,0.0);
+        glVertex3f(x, y, z);
+        glVertex3f(x + width, y, z);
+        glVertex3f(x + width, y + height, z);
+        glVertex3f(x, y + height, z);
     glEnd();
 }
 void Page::setMargin()
 {
+    glColor3f(0.0,0.0,0.0);
     glBegin(GL_LINE_LOOP);
-        glVertex3f(MARGIN_WIDTH,0,0.0);
-        glVertex3f(MARGIN_WIDTH,PAGE_HEIGHT,0.0);
+        glVertex3f(x + marginWidth, y, z);
+        glVertex3f(x + marginWidth, y + height, z);
     glEnd();
     glBegin(GL_LINES);
-        glVertex2f(0, PAGE_HEIGHT - MARGIN_HEIGHT);
-        glVertex2f(PAGE_HEIGHT, PAGE_HEIGHT - MARGIN_HEIGHT);
+        glVertex2f(x, y + height - marginHeight);
+        glVertex2f(x + width, x + height - marginHeight);
     glEnd();
 }
 void Page::setPageLines()
@@ -47,17 +61,18 @@ void Page::setPageLines()
         by LINE_HEIGHT
     */
     int index=0;
-    float pageBodyStartx = MARGIN_WIDTH;
-    float pageBodyStarty = PAGE_HEIGHT - MARGIN_HEIGHT;
-    float pageBodyEndx = PAGE_WIDTH; 
+    float pageBodyStartx = x + marginWidth;
+    float pageBodyStarty = y + height - marginHeight;
+    float pageBodyEndx = x + width; 
     glColor3f(1.0,0.0,0.0);
-    for(index=1;index<MAX_LINES && index<(pageBodyStarty)/LINE_HEIGHT;index++)
+    for(index=1;index<MAX_LINES && index<((pageBodyStarty-y)/(lineHeight));index++)
     {
-        lines[index] = (pageBodyStarty- (LINE_HEIGHT * index));
+        lines[index] = (pageBodyStarty - (LINE_HEIGHT * index));
         glBegin(GL_LINES);
-            glVertex3f(pageBodyStartx,lines[index],0.0);
-            glVertex3f(PAGE_WIDTH,lines[index],0.0);
+            glVertex3f(pageBodyStartx, lines[index],0.0);
+            glVertex3f(x + width, lines[index],0.0);
         glEnd();
     }
-    noOfLines=index-1; 
+    glColor3f(0.0,0.0,0.0);
+    noOfLines=index-1;
 }
--- flipbook.cpp
+++ flipbook.cpp
@@ -2,36 +2,41 @@
 #include<stdlib.h>
 #include<GL/glut.h>
 #include "constants.h"
-#include "Page.h"
 #include "Book.h"
 using namespace std;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
     glColor3f(0.0,0.0,0.0);
-    glPointSize(5.0);
-    Page page1(PAGE_TYPE_TEXT);
-    glColor3f(0.0,0.0,0.0);
+    Book flipbook(-100, -100, 0);
+    flipbook.addPage(PAGE_TYPE_TEXT);
+    flipbook.addPage(PAGE_TYPE_DRAWING);
+    flipbook.renderPage(1);
     glFlush();
+    glutSwapBuffers();
 }
-void initReshape()
+void initReshape(int w, int h)
 {
-    glViewport(0,0,PAGE_WIDTH,PAGE_HEIGHT);
-    glClearColor(1.0,1.0,1.0,0.0);
+    glViewport(0, 0, w, h);
+    glClearColor(1.0,1.0,1.0,1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-PAGE_WIDTH,PAGE_HEIGHT*2,-PAGE_HEIGHT,PAGE_WIDTH*2,-10.0,10.0);
+    //glOrtho(-PAGE_WIDTH,PAGE_HEIGHT*2,-PAGE_HEIGHT,PAGE_WIDTH*2,-10.0,10.0);
+    if(w<=h)
+        glOrtho(-WINDOW_WIDTH,WINDOW_WIDTH,-WINDOW_HEIGHT*(GLfloat)h/(GLfloat)w,WINDOW_HEIGHT*(GLfloat)h/(GLfloat)w,-10.0,10.0);
+    else
+        glOrtho(-WINDOW_WIDTH*(GLfloat)w/(GLfloat)h,WINDOW_WIDTH*(GLfloat)w/(GLfloat)h,-WINDOW_HEIGHT,WINDOW_HEIGHT,-10.0,10.0);
     glMatrixMode(GL_MODELVIEW);
     glutPostRedisplay();
 }
 int main(int argc, char **argv)
 {
     glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowPosition(50,50);
-    glutInitWindowSize(PAGE_WIDTH,PAGE_HEIGHT);
-    glutCreateWindow("Notebook");
-    initReshape();
+    glutInitWindowSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+    glutCreateWindow("FlipBook");
+    glutReshapeFunc(initReshape);
     glutDisplayFunc(display);
     glEnable(GL_DEPTH_TEST);
     glutMainLoop();
